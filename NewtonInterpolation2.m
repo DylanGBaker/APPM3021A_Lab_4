55674f44a87cf8350600bb6b061519cfb4781333
Skaduvee <61471350+Skaduvee@users.noreply.github.com>
2023-05-05 17:32:42 +0200

    removed disp()

--- NewtonInterpolation2.m
+++ NewtonInterpolation2.m
@@ -7,13 +7,10 @@ for i = 1:size(xq,1)
         temp = coefficientMatrix(j,1);
         k = 1;
         while k < j
-            %disp(xq(i,1));
-            %disp(x(k,1));
             temp = temp * (xq(i,1) - x(k,1));
             k = k + 1;
         end
         yq(i,1) = yq(i,1) + temp;
-        %disp(yq(i,1));
     end
 end
 end
